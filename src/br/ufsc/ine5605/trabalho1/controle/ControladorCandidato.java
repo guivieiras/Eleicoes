9f44a5d3be20a31bfe754923fc78964128ef45bd
The Gui <The Gui@marcia-PC>
2016-10-14 16:50:03 -0300

    Fim 2.0001

--- src/br/ufsc/ine5605/trabalho1/controle/ControladorCandidato.java
+++ src/br/ufsc/ine5605/trabalho1/controle/ControladorCandidato.java
@@ -22,7 +22,7 @@ public class ControladorCandidato implements IControlador<Candidato> {
                 return false;
             }
         }
-        if (candidato.getNumero() > 98 || candidato.getNumero() < 2)
+        if (candidato.getNumero() > 98 || candidato.getNumero() < 1)
             return false;
 
         return candidatos.add(candidato);
